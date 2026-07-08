f823f1e4e1ae26c176ce585810b299f25b3cdc39
m-mpk <m_ma@mzinx.com>
2026-07-09 02:32:45 +0800

    bean init

--- src/main/java/com/mzinx/mongodb/aggregation/config/AggregationAutoConfig.java
+++ src/main/java/com/mzinx/mongodb/aggregation/config/AggregationAutoConfig.java
@@ -1,10 +1,16 @@
 package com.mzinx.mongodb.aggregation.config;
 
+import org.bson.codecs.configuration.CodecRegistries;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
+
+import com.mongodb.MongoClientSettings;
 
 @AutoConfiguration
 @EnableConfigurationProperties(AggregationProperties.class)
@@ -13,4 +19,10 @@ import org.springframework.context.annotation.Import;
 @Import(ScanRegistrar.class)
 public class AggregationAutoConfig {
 
+    @Bean
+    public CodecRegistry pojoCodecRegistry() {
+        return CodecRegistries.fromRegistries(
+                MongoClientSettings.getDefaultCodecRegistry(),
+                CodecRegistries.fromProviders(PojoCodecProvider.builder().automatic(true).build()));
+    }
 }
