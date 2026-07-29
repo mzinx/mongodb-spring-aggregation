86788bd8a0f29ee450848687dad1a3e1f2ac2f23
M Ma <V313ABC+cpa@Cathaypacific.com>
2026-07-30 02:31:08 +0800

    Back off pojoCodecRegistry when the host defines a CodecRegistry bean

--- src/main/java/com/mzinx/mongodb/aggregation/config/AggregationAutoConfig.java
+++ src/main/java/com/mzinx/mongodb/aggregation/config/AggregationAutoConfig.java
@@ -4,6 +4,7 @@ import org.bson.codecs.configuration.CodecRegistries;
 import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.codecs.pojo.PojoCodecProvider;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -19,7 +20,12 @@ import com.mongodb.MongoClientSettings;
 @Import(AutoConfigurationPackageRegistrar.class)
 public class AggregationAutoConfig {
 
+    /**
+     * Default POJO codec registry; backs off when the host application
+     * defines its own {@link CodecRegistry} bean.
+     */
     @Bean
+    @ConditionalOnMissingBean(CodecRegistry.class)
     public CodecRegistry pojoCodecRegistry() {
         return CodecRegistries.fromRegistries(
                 MongoClientSettings.getDefaultCodecRegistry(),
